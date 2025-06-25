22212f1ce5bdb60144b4ef9ffde381e4e4b6130f
Joyce8900 <joycesantos8900@gmail.com>
2025-06-25 17:38:43 -0300

    feat(foto): crud completo utilizando generic views #1

--- ProjetoSAC/medico/urls.py
+++ ProjetoSAC/medico/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from medico.views import MedicoListView, MedicoCreateView  # Importe diretamente da app
+from medico.views import MedicoListView, MedicoCreateView, MedicoUpdateView, MedicoDetailView, MedicoDeleteView 
 
 urlpatterns = [
     path('', MedicoListView.as_view(), name='medicos'),
     path('cadastro/', MedicoCreateView.as_view(), name='cadastro'),
+    path('editar/<str:crm>/', MedicoUpdateView.as_view(), name='editar'),
+    path('detalhe/<str:crm>/', MedicoDetailView.as_view(), name='detalhe'),
+    path('excluir/<int:pk>/', MedicoDeleteView.as_view(), name='excluir'),
 ]
--- ProjetoSAC/medico/views.py
+++ ProjetoSAC/medico/views.py
@@ -1,9 +1,9 @@
+from datetime import timezone
 from django.shortcuts import render
-from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView, DetailView, DeleteView
 from .models import Medico
 from .forms import MedicoForm
-
+URL = '/medicos/'
 # Create your views here.
 
 class MedicoListView(ListView):
@@ -15,4 +15,28 @@ class MedicoCreateView(CreateView):
     model = Medico
     form_class = MedicoForm
     template_name = 'cadastro.html'
-    success_url = reverse_lazy('medicos') 
+    success_url = URL # Redirect after successful creation
+    
+
+class MedicoUpdateView(UpdateView):
+    model = Medico
+    form_class = MedicoForm
+    template_name = 'editar.html'
+    success_url = URL
+    slug_field = 'crm'
+    slug_url_kwarg = 'crm'
+
+class MedicoDetailView(DetailView):
+    model = Medico
+    template_name = 'detalhe.html'
+    context_object_name = URL
+    slug_field = 'crm'
+    slug_url_kwarg = 'crm'
+
+class MedicoDeleteView(DeleteView):
+    model = Medico
+    template_name = 'excluir.html'
+    slug_field = 'crm'
+    slug_url_kwarg = 'crm'
+    success_url = URL
+    
