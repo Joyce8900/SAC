92b9c7ad1cc68a123dcc826780669a405686a7c2
Gustavo-DSC <gustavodouglas646@gmail.com>
2025-07-13 21:52:50 -0300

    refactor: ajustar URLs e views de usuários #7

--- ProjetoSAC/usuarios/urls.py
+++ ProjetoSAC/usuarios/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from .views import Cadastro, Login, Home
-
 
 urlpatterns = [
     path('cadastro/', Cadastro.as_view(), name='cadastro'),
-    path('login/', Login.as_view(), name='login'),
+    path('', Login.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('home/', Home.as_view(), name='home'),
 ]
