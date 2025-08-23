bf86a320ebf5e8f3068e88f0f519203c17ccfe91
Joyce8900 <joycesantos8900@gmail.com>
2025-08-23 16:21:52 -0300

    Corrigido bug de erro ao exibir imagem.

--- ProjetoSAC/ProjetoSAC/urls.py
+++ ProjetoSAC/ProjetoSAC/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,3 +14,5 @@ urlpatterns = [
     path('usuarios/', include('usuarios.urls')),
     path('consulta/', include('consulta.urls', namespace='consulta')),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
